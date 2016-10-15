2146ecd94b5b29d5ad4048f3f8a5d65cc67f7345
Phil Gibbins <scoobster17@hotmail.co.uk>
2016-10-15 12:22:23 +0100

    Initial commit, windows not working

---
+++ README.md
@@ -0,0 +1,11 @@
+# PhilAndSteph.co.uk Source Code
+
+I have decided to re-write this site from PHP into a React app.
+
+Upon speaking to my friend @shedali he has inspired me to expose more
+if not all of what I do, and therefore code I write.
+
+So here it is, the new and improved version of my wedding website,
+PhilAndSteph.co.uk.
+
+** LIVE version not yet using this repository **
---
+++ server/config.js
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+
+/**
+ * Server config for Phil and Steph wedding website project
+ * Author: Phil Gibbins
+ */
+
+/* DEPENDENCIES */
+
+// node framework
+var express = require('express');
+
+// sets up environment variables
+require('dotenv').config()
+
+/* ************************************************************************** */
+
+/* APP SETUP */
+
+var app = express();
+app.use( express.static(__dirname + "/../dist/") );
+
+/* ************************************************************************** */
+
+/* SERVER */
+
+var server = app.listen(process.env.APP_UI_PORT, function() {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Wedding website app listening @ http://%s:%s", host, port);
+
+});
+
+/* ************************************************************************** */
