0f31174b277bd178d71bcec97ac2f60ef99979a2
Phil Gibbins <scoobster17@hotmail.co.uk>
2016-12-29 20:41:59 +0000

    Fix for 404 page, re-instated all existing routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,12 +5,23 @@ import { Route, IndexRoute } from 'react-router';
 // Pages (and container 'Layout' component)
 import Layout from './layout';
 import HomePage from '../components/pages/home';
+import EventsPage from '../components/pages/events';
+import PeoplePage from '../components/pages/people';
+import MediaPage from '../components/pages/media';
+import NewsPage from '../components/pages/news';
 import ContactPage from '../components/pages/contact';
+import NotFoundPage from '../components/pages/404';
+
 
 const routes = (
     <Route path="/" component={Layout}>
         <IndexRoute component={HomePage} />
+        <Route path="people" component={PeoplePage} />
+        <Route path="people/:personId" component={PeoplePage} />
+        <Route path="media" component={MediaPage} />
+        <Route path="news" component={NewsPage} />
         <Route path="contact" component={ContactPage} />
+        <Route path="*" component={NotFoundPage} />
     </Route>
 )
 
