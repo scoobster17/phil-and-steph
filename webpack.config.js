c576df44627bb201bc6e0b99dd941b4341f0aab2
Phil Gibbins <scoobster17@hotmail.co.uk>
2017-01-04 19:08:47 +0000

    Consolidated webpack modules (client/server) into one file, so only need one watch process

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const fs = require('fs');
+
+let nodeModules = {};
+fs.readdirSync('node_modules')
+	.filter(x => { return ['bin'].indexOf(x) === -1 })
+	.forEach(mod => { nodeModules[mod] = 'commonjs ' + mod });
 
 module.exports = [
 	{
@@ -65,6 +71,44 @@ module.exports = [
 			}),
 			new ExtractTextPlugin('../css/[name].css', {
 				allChunks: true
+			})
+		]
+	},
+	{
+		devtool: 'source-map',
+		entry: path.join(__dirname, 'src', 'server.js'),
+		target: 'node',
+		node: {
+			__dirname: true
+		},
+		externals: nodeModules,
+		output: {
+			path: path.join(__dirname, 'build'),
+			filename: 'server.js'
+		},
+		module: {
+			loaders: [
+				{
+					test: path.join(__dirname, 'src'),
+					loader: ['babel-loader'],
+					query: {
+						presets: ['react', 'es2015']
+					}
+				}
+			]
+		},
+		plugins: [
+			new webpack.DefinePlugin({
+				'process.env.NODE_ENV': JSON.stringify('production')
+			}),
+			new webpack.ProvidePlugin({
+				React: 'react',
+				ReactDOM: 'react-dom'
+			}),
+			new webpack.optimize.UglifyJsPlugin({
+				compress: {
+					warnings: false
+				}
 			})
 		]
 	}
