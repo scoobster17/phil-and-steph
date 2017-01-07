a6ae20945dc31fa683079665ab8edd54dbddef73
Phil Gibbins <scoobster17@hotmail.co.uk>
2017-01-07 22:24:16 +0000

    Added gift registry list details

--- src/data/news.js
+++ src/data/news.js
@@ -6,7 +6,7 @@ const news = [
 		"urlText": "honeymoon-booked",
 		"accessibiltyDescription": "the honeymoon being booked",
 		"preview": "The honeymoon has now been booked! We are soaring off to our chosen destination of Jamaica&hellip;",
-		"content": "<p>The honeymoon has now been booked! We are soaring off to our chosen destination of Jamaica soon after the wedding for a lovely all&ndash;inclusive trip of a lifetime!</p><p>More here</p>"
+		"content": "<p>The honeymoon has now been booked! We are soaring off to our chosen destination of Jamaica soon after the wedding for a lovely all&ndash;inclusive trip of a lifetime!</p><p>More to follow...</p>"
 	},
 	{
 		"id": 1,
@@ -15,7 +15,7 @@ const news = [
 		"urlText": "site-revamp",
 		"accessibiltyDescription": "this site being updated",
 		"preview": "This site has finally been updated! New features include the News section, Events section and more&hellip;",
-		"content": "<p>Welcome to the renewed and slightly refreshed version of the PhilAndSteph.co.uk website! Although it may not look all that different, it has been re-built from the ground up!</p><p>More here</p>"
+		"content": "<p>Welcome to the renewed and slightly refreshed version of the PhilAndSteph.co.uk website! Although it may not look all that different, it has been re-built from the ground up!</p><p>More to follow...</p>"
 	},
 	{
 		"id": 2,
@@ -24,7 +24,16 @@ const news = [
 		"urlText": "registrar-booked",
 		"accessibiltyDescription": "the registrar being booked",
 		"preview": "The registrar has been booked. So now it is official, Steph is fencing Phil in! It's officially happening now!",
-		"content": "<p>The registrar has been booked and fully paid for, so the wedding is now officially going ahead!</p><p>More here</p>"
+		"content": "<p>The registrar has been booked and fully paid for, so the wedding is now officially going ahead!</p><p>More to follow...</p>"
+	},
+	{
+		"id": 3,
+		"timestamp": 1476826234727,
+		"title": "Honeymoon site / Wedding Gift List Set Up",
+		"urlText": "gift-list-set-up",
+		"accessibiltyDescription": "the gift list being set up",
+		"preview": "We have set up our Wedding Gift List. We don't expect it, but if you feel obliged to get us a wedding gift, find out the details here.",
+		"content": "<p>The wedding gift list has been set up, and you can now view it here at <a href=\"https://www.ourdreamhoneymoon.com/phil-and-steph\">www.ourdreamhoneymoon.com/phil-and-steph</a></p><p>More to follow...</p>"
 	}
 ];
 
