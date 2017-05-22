192b3c063371d04bca0b822578a4bb3a2784ccdb
Phil Gibbins <scoobster17@hotmail.co.uk>
2017-05-22 22:26:22 +0100

    Alteration so honeymoon not mentioned as paid

--- src/data/news.js
+++ src/data/news.js
@@ -80,11 +80,11 @@ const news = [
 	{
 		"id": 6,
 		"timestamp": 1476826234730,
-		"title": "Venue and Honeymoon paid",
+		"title": "Venue and registrar paid",
 		"urlText": "its-happening",
 		"accessibilityDescription": "the fact that the wedding is definitely set to go.",
-		"preview": "Both the venue (+ registrar if you're being picky!) and the honeymoon payments have been made so the day is definitely going to happen!",
-		"content": "<section class=\"panel\"><p>That's it, it's definitely happening. Venue? Paid. Registrar? Paid. Honeymoon? Paid. Sorted! We are all set now to get married so make sure you book that time off! And perhaps the Tuesday too if you plan on it being a heavy one!</p><p>We look forward to seeing you, our guests, on our special day, and sharing in our commitment to one another. And then get jealous of our lovely trip away which is also definitely happening now! So excited! (Don't tell Steph but I'm more excited about that part!).</p><p>You can still contribute to our wedding gift list to contribute part of the honeymoon here at <a href=\"https://www.ourdreamhoneymoon.com/phil-and-steph\" target=\"_blank\" class=\"textLink\">www.ourdreamhoneymoon.com/phil-and-steph</a>.</p><img src=\"/img/wedding/misc/paid.jpg\" alt=\"A 'Paid' stamp as if stamped across a paper document\" /></section>",
+		"preview": "Both the venue and registrar payments have been made so the day is definitely going to happen!",
+		"content": "<section class=\"panel\"><p>That's it, it's definitely happening. Venue? Paid. Registrar? Paid. Sorted! We are all set now to get married so make sure you book that time off! And perhaps the Tuesday too if you plan on it being a heavy one!</p><p>We look forward to seeing you, our guests, on our special day, and sharing in our commitment to one another. (Don't tell Steph but I'm more excited about the honeymoon!)</p><p>You can still contribute to our wedding gift list as part of the honeymoon (some items have already been reserved!) here at <a href=\"https://www.ourdreamhoneymoon.com/phil-and-steph\" target=\"_blank\" class=\"textLink\">www.ourdreamhoneymoon.com/phil-and-steph</a>.</p><img src=\"/img/wedding/misc/paid.jpg\" alt=\"A 'Paid' stamp as if stamped across a paper document\" /></section>",
 		"img": {
 			"src": "wedding/misc/thumb-paid.jpg",
 			"alt": "A 'Paid' stamp as if stamped across a paper document"
